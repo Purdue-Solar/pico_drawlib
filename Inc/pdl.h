cb56c4936f427cae9e3ded70ff087e33675e1956
Tsundoiii <dev@tsundoiii.dev>
2026-03-28 10:53:35 -0400

    add can implementation for display

---
+++ Inc/pdl.h
@@ -0,0 +1,74 @@
+#ifndef PDL_H
+#define PDL_H
+
+#include "mcufont.h"
+
+/*
+
+|---------------------------------320--------------------------------|
+
+|--------80--------|--------------160-------------|--------80--------|
+
+----------------------------------------------------------------------  -  -
+| MAIN             |                              |             AUX  |  |  |
+|                  |                              |                  |  |  |
+|                  |                              |                  |  |  |
+|                  |                              |                  |  2  |
+|                  |                              |                  |  0  |
+|                  |                              |                  |  0  |
+|                  |                              |                  |  |  |
+|                  |                              |                  |  |  2
+|                  |                              |                  |  |  4
+|                  |                              |                  |  |  0
+|                  |                              |                  |  |  |
+|                  |                              |                  |  |  |
+|                  |                              |                  |  |  |
+|--------------------------------------------------------------------|  -  |
+| Warn |                                Warning                      |  4  |
+| Icon |                                or Error Message             |  0  |
+----------------------------------------------------------------------  -  -
+
+|--40--|----------------------------280------------------------------|
+*/
+
+typedef struct PDLInfo
+{
+    uint8_t battery_temperature;
+    uint8_t battery_soc;
+
+    uint32_t motor_current;
+    uint32_t motor_velocity;
+
+    bool aux_over_voltage;
+    bool aux_under_voltage;
+    bool aux_over_current;
+    bool aux_current_warning;
+
+    bool main_over_voltage;
+    bool main_under_voltage;
+    bool main_over_current_error;
+    bool main_current_warning;
+    // uint8_t aux_condition; // 4 bits
+    bool main_valid; // DC valid
+    bool aux_valid;
+
+    // uint32_t velocity;
+
+    // uint32_t motor_temperature;
+
+    // unsigned int aux_current;
+    // unsigned int aux_voltage;
+} PDLInfo;
+
+#define PDL_WIDTH 320
+#define PDL_HEIGHT 240
+#define PDL_WARNING_HEIGHT 40
+#define PDL_CENTERPANEL_WIDTH 160
+
+#define PDL_CENTERPANEL_LEFT  ((PDL_WIDTH - PDL_CENTERPANEL_WIDTH) / 2.0)
+#define PDL_CENTERPANEL_RIGHT (PDL_CENTERPANEL_LEFT + PDL_CENTERPANEL_WIDTH)
+#define PDL_MAIN_BOTTOM (PDL_HEIGHT - PDL_WARNING_HEIGHT)
+
+void pdl_draw(const PDLInfo* info);
+
+#endif // !PDL_H
---
+++ Src/main.cpp
@@ -0,0 +1,100 @@
+#include <stdint.h>
+
+#include "ili9341.h"
+
+#ifdef SIMULATION
+#include "picosdk_sim.h"
+#else
+#include "pico/stdio.h"
+#include "pico/time.h"
+#include "hardware/gpio.h"
+#include "pico_canlib.hpp"
+#include "artemis_canid.hpp"
+#include "pdl.h"
+#endif
+
+#include <stdio.h>
+#include <cstring>
+
+#define CAN_ID 0x
+
+int main()
+{
+    Info info;
+    uint8_t buffer[13];
+    uint32_t id;
+    uint8_t data[8];
+
+    stdio_init_all();
+    pico_canlib can = pico_canlib();
+    can.init();
+
+    bool led_on = true;
+
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
+
+    gpio_init(22);
+    gpio_set_dir(22, GPIO_OUT);
+    gpio_put(22, led_on);
+
+    LCD_setPins(20, 17, 21, 18, 19);
+    LCD_setSPIperiph(spi0);
+    LCD_initDisplay();
+    LCD_setRotation(1);
+
+#ifdef SIMULATION
+    LCDSim_InitWindow();
+
+    while (!LCDSim_WindowShouldClose())
+#else
+    while (true)
+#endif
+    {
+        if (can.receiveCAN(buffer, 4, 8) == pico_canlib::status::SUCCESS)
+        {
+            id = buffer[0] << 24 | buffer[1] << 16 | buffer[2] << 8 | buffer[3];
+
+            for (int i = 0; i < 8; i++)
+            {
+                data[i] = buffer[5 + i];
+            }
+
+            switch (id)
+            {
+            case artemis_canid::tempAndSOC:
+                info.battery_temperature = data[7];
+                info.battery_soc = data[4];
+                break;
+            case artemis_canid::setMotorCurrent:
+                info.motor_current = data[7] << 24 | data[6] << 16 | data[5] << 8 | data[4];
+                break;
+            case artemis_canid::motorVelocity:
+                info.motor_velocity = data[7] << 24 | data[6] << 16 | data[5] << 8 | data[4];
+                break;
+            case artemis_canid::steeringWheel:
+                info.main_valid = data[0] & 0b00000001;
+                info.aux_valid = data[0] & 0b00000010;
+                info.main_current_warning = data[0] & 0b00010000;
+                info.main_over_current_error = data[0] & 0b00100000;
+                info.main_under_voltage = data[0] & 0b01000000;
+                info.main_over_voltage = data[0] & 0b10000000;
+
+                info.aux_current_warning = data[1] & 0b00000001;
+                info.aux_over_current = data[1] & 0b00000010;
+                info.aux_under_voltage = data[1] & 0b00000100;
+                info.aux_over_voltage = data[1] & 0b00001000;
+
+                break;
+            }
+        }
+#ifdef SIMULATION
+        LCDSim_Redraw();
+#endif
+    }
+
+#ifdef SIMULATION
+    LCDSim_CloseWindow();
+#endif
+}
